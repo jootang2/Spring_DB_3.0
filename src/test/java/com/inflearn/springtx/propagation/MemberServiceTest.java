92741b237e8bd7e12b27b7fa653dfa4dd712c125
joohwan <joohwan9607@mobility42.io>
2023-03-27 18:55:20 +0900

    Tx 전파 - example code 1

---
+++ src/test/java/com/inflearn/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,36 @@
+package com.inflearn.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    /**
+     * memberService @Transactional:Off
+     * memberRepository @Transcactional:ON
+     * logRepository @Transcactional:ON
+     */
+    @Test
+    void outerTxOff_success() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+}
